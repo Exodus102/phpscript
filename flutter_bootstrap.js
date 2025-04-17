9ad7cc062263aebf1629617326c00fad389a2c0f
Exodus102 <145414466+Exodus102@users.noreply.github.com>
2025-04-17 09:45:26 +0800

    Version 1.2
    Version 1.2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "929714566"
+    serviceWorkerVersion: "4046225734"
   }
 });
